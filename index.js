f8dafd71b4a8b6226eb9874618b308ee1a68dd8d
shubh <shubhamshukla.shukla@gmail.com>
2025-01-31 23:38:09 +0530

    changed ui and updated packages

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var app=require('express')();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
 
+const funnyResponses = [
+  "Why don't skeletons fight each other? They don't have the guts!",
+  "I used to play piano by ear, but now I use my hands!",
+  "I'm on a whiskey diet... I’ve lost three days already!",
+  "Why don’t oysters donate to charity? Because they are shellfish.",
+  "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+  "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+  "I'm reading a book on anti-gravity. It's impossible to put down!",
+  "I told my computer I needed a break, and now it won't stop sending me Kit-Kats!"
+];
+
 
 
 
@@ -13,7 +24,9 @@ io.on('connection',function(socket) {
     console.log("a user connected");
 
      socket.on('chat message',function(message) {
-        io.emit('chat message', message);
+        io.emit('chat message event', message);
+        const randomIndex = Math.floor(Math.random() * funnyResponses.length);
+        io.emit('chat message event', funnyResponses[randomIndex]);
      })
 
      socket.on('disconnect', function(){
